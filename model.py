cffe40cbbc1580ca6db85a792cc2b9699eda9d1e
sanitgupta <sanitsgupta@gmail.com>
2020-06-14 17:58:23 +0530

    Risk

--- model.py
+++ model.py
@@ -17,28 +17,44 @@ def compute_gini(model):
 
 class MoneyAgent(Agent):
 
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, risky, init_wealth = 100):
+        
         super().__init__(unique_id, model)
-        self.wealth = 100
+        self.wealth = init_wealth
+        self.risky = risky
 
     def step(self):
 
         if self.wealth == 0:
             return
-        
-        if self.random.random() < 0.5:
-            self.wealth += (self.wealth / 2) /2 
+
+        if self.risky:
+            if self.random.random() < 0.5:
+                self.wealth += (self.wealth / 2) / 2 
+            else:
+                self. wealth -= self.wealth / 2
         else:
-            self. wealth -= self.wealth / 2
+            if self.random.random() < 0.9:
+                self.wealth += self.wealth / 20
+            else:
+                self.wealth -= self.wealth / 10
+
 
 class MoneyModel(Model):
 
-    def __init__(self, N):
+    def __init__(self, N, risky_frac = 0.5):
+
         self.num_agents = N
         self.schedule = RandomActivation(self)
+        self.risky_frac = risky_frac
 
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+            if self.random.random() < risky_frac:
+                risky = True
+            else:
+                risky = False
+            
+            a = MoneyAgent(i, self, risky)
             self.schedule.add(a)
 
 
@@ -49,11 +65,11 @@ class MoneyModel(Model):
 
 
     def step(self):
-        '''Advance the model by one step.'''
+
         self.datacollector.collect(self)
         self.schedule.step()
 
-model = MoneyModel(1000)
+model = MoneyModel(10000)
 
 for i in range(20):
     model.step()
@@ -62,8 +78,14 @@ gini = model.datacollector.get_model_vars_dataframe()
 gini.plot()
 plt.show()
 
-agent_wealth = [a.wealth for a in model.schedule.agents]
-print(max(agent_wealth))
+risky_agent_wealth = [a.wealth for a in model.schedule.agents if a.risky == True]
+print("Risk Takers (max and mean wealth at the end): ", max(risky_agent_wealth), np.mean(risky_agent_wealth))
+plt.hist(risky_agent_wealth)
+plt.show()
 
-plt.hist(agent_wealth)
+
+notrisky_agent_wealth = [a.wealth for a in model.schedule.agents if a.risky == False]
+
+print("Risk Avoiders (max and mean wealth at the end): ", max(notrisky_agent_wealth), np.mean(notrisky_agent_wealth))
+plt.hist(notrisky_agent_wealth)
 plt.show()
