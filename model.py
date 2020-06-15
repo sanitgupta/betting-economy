c18ed55754d4c2e2d18fe2c6d2da3f5931728271
sanitgupta <sanitsgupta@gmail.com>
2020-06-15 15:48:03 +0530

    Rich

--- model.py
+++ model.py
@@ -17,22 +17,28 @@ def compute_gini(model):
 
 class MoneyAgent(Agent):
 
-    def __init__(self, unique_id, model, risky, init_wealth = 100):
+    def __init__(self, unique_id, model, risky, rich, base_wealth = 100):
         
         super().__init__(unique_id, model)
-        self.wealth = init_wealth
+        
+        self.wealth = base_wealth
         self.risky = risky
+        self.rich = rich
+        self.base_wealth = base_wealth
+
+        if self.rich:
+            self.wealth = 10 * base_wealth
 
     def step(self):
 
-        if self.wealth == 25:
+        if self.wealth < self.base_wealth / 4:
             return
 
         if self.risky:
             if self.random.random() < 0.5:
-                self.wealth += (self.wealth / 2) / 2 
+                self.wealth += self.wealth / 4 
             else:
-                self. wealth -= self.wealth / 2
+                self.wealth -= self.wealth / 2
         else:
             if self.random.random() < 0.9:
                 self.wealth += self.wealth / 20
@@ -42,19 +48,25 @@ class MoneyAgent(Agent):
 
 class MoneyModel(Model):
 
-    def __init__(self, N, risky_frac = 0.5):
+    def __init__(self, N, risky_frac = 0.5, rich_frac = 0.5):
 
         self.num_agents = N
         self.schedule = RandomActivation(self)
         self.risky_frac = risky_frac
 
         for i in range(self.num_agents):
+
             if self.random.random() < risky_frac:
                 risky = True
             else:
                 risky = False
+
+            if self.random.random() < rich_frac:
+                rich = True
+            else:
+                rich = False
             
-            a = MoneyAgent(i, self, risky)
+            a = MoneyAgent(i, self, risky, rich)
             self.schedule.add(a)
 
 
@@ -69,7 +81,7 @@ class MoneyModel(Model):
         self.datacollector.collect(self)
         self.schedule.step()
 
-model = MoneyModel(10000)
+model = MoneyModel(100000)
 
 for i in range(20):
     model.step()
@@ -78,14 +90,20 @@ gini = model.datacollector.get_model_vars_dataframe()
 gini.plot()
 plt.show()
 
-risky_agent_wealth = [a.wealth for a in model.schedule.agents if a.risky == True]
-print("Risk Takers (max and mean wealth at the end): ", max(risky_agent_wealth), np.mean(risky_agent_wealth))
-plt.hist(risky_agent_wealth)
+rr = [a.wealth for a in model.schedule.agents if a.risky == True and a.rich == True]
+sr = [a.wealth for a in model.schedule.agents if a.risky == False and a.rich == True]
+rp = [a.wealth for a in model.schedule.agents if a.risky == True and a.rich == False]
+sp = [a.wealth for a in model.schedule.agents if a.risky == False and a.rich == False]
+
+print("Risky Rich: ", max(rr), np.mean(rr))
+print("Safe Rich: ", max(sr), np.mean(sr))
+print("Risky Poor: ", max(rp), np.mean(rp))
+print("Safe Poor: ", max(sp), np.mean(sp))
+
+
+
+plt.hist(rr)
 plt.show()
 
 
-notrisky_agent_wealth = [a.wealth for a in model.schedule.agents if a.risky == False]
 
-print("Risk Avoiders (max and mean wealth at the end): ", max(notrisky_agent_wealth), np.mean(notrisky_agent_wealth))
-plt.hist(notrisky_agent_wealth)
-plt.show()
